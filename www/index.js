70c86479aeb3dfd944421bfd6143d5be6e05aaa8
Kevin Jahns <kevin.jahns@protonmail.com>
2020-10-21 00:57:19 +0200

    update to work with latest yrs build

--- www/index.js
+++ www/index.js
@@ -1,21 +1,23 @@
 import * as wasm from 'ywasm'
 
-const start = performance.now()
+let start = performance.now()
 const doc = new wasm.Doc()
 const ytext = doc.getType('type')
 const tr = doc.transact()
-for (let i = 0; i < 10; i++) {
-  const pos = Math.floor(Math.random() * (i + 1))
-  ytext.insert(tr, pos, (i % 10).toString())
+for (let i = 0; i < 1000000; i++) {
+  ytext.insert(tr, 0, (i % 10).toString())
 }
 
-console.log('time preps: ', performance.now() - start)
-console.log('text: ', ytext.toString())
+console.log('time to apply prepends: ', performance.now() - start)
+console.log('text length: ', ytext.toString().length)
 const encodedState = doc.encodeStateAsUpdate()
-console.log(encodedState)
+console.log('update message size: ', encodedState.length)
 
 const doc2 = new wasm.Doc()
 const ytext2 = doc.getType('type')
 
+start = performance.now()
 doc2.applyUpdate(encodedState)
-console.log(ytext2.toString())
+
+console.log('time to apply update message: ', performance.now() - start)
+console.log('text length: ', ytext2.toString().length)
